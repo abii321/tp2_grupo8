53d1ab64929271757cc026b34042e734b333c19c
Abigail <abigail.teran321@gmail.com>
2025-08-31 19:55:07 -0300

    Ejercicios 5, 10, 15 y 20 - Abigail Teran

--- src/ar/edu/unju/escmi/tp2/ejercicio15/Ejercicio15.java
+++ src/ar/edu/unju/escmi/tp2/ejercicio15/Ejercicio15.java
@@ -6,11 +6,11 @@ public class Ejercicio15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int n;
-        do {
+        int n=0;
+        do{
             System.out.println("Ingrese un número: ");
             n = sc.nextInt();
-         } while(n<3 && n<10);
+        } while(n<3 || n>10);
 
         int[] arr = new int[n];
 
@@ -21,7 +21,11 @@ public class Ejercicio15 {
         }
         sc.close();
 
-        for(int i=0; i<n; i++) 
+        int suma=0;
+        for(int i=0; i<n; i++){
             System.out.println("Posicion: " + i + ", Valor: " + arr[i]);
+            suma+=arr[i];
+        }
+        System.out.println("La suma de todos los valores del array es "+suma);
     }
 }
--- src/ar/edu/unju/escmi/tp2/ejercicio20/Ejercicio20.java
+++ src/ar/edu/unju/escmi/tp2/ejercicio20/Ejercicio20.java
@@ -8,3 +8,9 @@ public class Ejercicio20 {
         System.out.println("textoModificado = " + textoModificado);
     }
 }
+
+/*  ¿Por qué el valor de textoOriginal no cambia después de llamar al método replace()? 
+    El valor de textoOriginal no cambia porque los strings son inmutables. 
+    Entonces, cuando se ejecuta replace, no se modifica el objeto original, sino que se crea un nuevo objeto String con los cambios. 
+    El valor de textoOriginal sigue siendo "Nombre", y a textoModificado se le asigna una direccion de memoria distinta, con su propio valor.
+ */
