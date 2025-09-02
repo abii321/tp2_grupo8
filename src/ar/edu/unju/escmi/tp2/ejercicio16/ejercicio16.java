f5f403c353f1db5113d3b05b8d73cee218afd259
lujancansino <celestelujancansino@gmail.com>
2025-09-02 08:26:33 -0300

    ejercicios-1-6-11-16

---
+++ src/ar/edu/unju/escmi/tp2/ejercicio16/ejercicio16.java
@@ -0,0 +1,34 @@
+package ar.edu.unju.escmi.tp2.ejercicio16;
+
+import java.util.Scanner;
+
+public class Ejercicio16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        do {
+            System.out.print("Ingrese un número entre 5 y 10: ");
+            n = sc.nextInt();
+        } while (n < 5 || n > 10);
+
+        String[] nombres = new String[n];
+
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.print("Ingrese el nombre " + (i + 1) + ": ");
+            nombres[i] = sc.next();
+        }
+
+        System.out.println("\nMostrando nombres desde el índice 0:");
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println("Posición " + i + ": " + nombres[i]);
+        }
+
+        System.out.println("\nMostrando nombres desde la última posición:");
+        for (int i = nombres.length - 1; i >= 0; i--) {
+            System.out.println("Posición " + i + ": " + nombres[i]);
+        }
+
+        sc.close();
+    }
+}
---
+++ src/ar/edu/unju/escmi/tp2/ejercicio6/ejercicio6.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.escmi.tp2.ejercicio6;
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese el radio del círculo (byte): ");
+        byte radio = sc.nextByte();
+
+        double area = Math.PI * Math.pow(radio, 2);
+
+        System.out.println("El área del círculo es: " + area);
+        sc.close();
+    }
+}
